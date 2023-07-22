59c9c7daf4ab9aadb00f4a5fc02cd072f716023a
João Victor Gadelha <108595157+victorgadelha@users.noreply.github.com>
2023-07-22 03:00:50 -0300

    feat: tentando colocar as imagens nos cards

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,8 +1,21 @@
+const cardNews = document.querySelector('.content__news')
+
 const API_URL = 'https://newsapi.org/v2/top-headlines?category=technology&apiKey=8a7f37754729406887c42dac68fb9edf'
-const fetchData = async () => {
+const fetchNews = async () => {
     const response = await fetch(API_URL)
     const data = await response.json()
-    console.log(data)
+    return data
 }
 
-fetchData()
+const setNews = async () => {
+    const data = await fetchNews()
+    const image = await data.articles[5].urlToImage
+
+    
+    console.log(data.articles[5].urlToImage)
+}
+
+setNews()
+
+
+
