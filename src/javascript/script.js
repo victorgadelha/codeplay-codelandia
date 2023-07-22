2b61ca7a73b2728a4a07bbbf3908e7856e55e9a3
João Victor Gadelha <108595157+victorgadelha@users.noreply.github.com>
2023-07-22 17:16:09 -0300

    feat: finalizando estrutura do código

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -63,4 +63,67 @@ const setNews = async () => {
 
 setNews()
 
+/*const mainNews = document.querySelector('.content')
 
+const API_URL = 'https://api.nytimes.com/svc/topstories/v2/technology.json?api-key=n8dV40S2ybMt90xQYjvSwFpxfMsF7rS4'
+const fetchNews = async () => {
+    const response = await fetch(API_URL)
+    const data = await response.json()
+    console.log(data)
+}
+
+const setNews = async () => {
+    allNews = await fetchNews()
+    tecNews = allNews.results
+    
+    try {
+        for (let i = 0; i < tecNews.length; i++) {
+            const newsImage = tecNews[i].urlToImage
+            const newsDate = tecNews[i].publishedAt.substr(0, 10)
+            const newsTitle = tecNews[i].title
+            const newsLink = tecNews[i].url
+    
+           // if (!newsImage || !newsDate || !newsTitle) {
+                //continue;
+            //}
+    
+            const newsDateContainer = document.createElement('p')
+            newsDateContainer.classList.add('content__text-date')
+            newsDateContainer.textContent = newsDate
+    
+            const newsTitleContainer = document.createElement('h2')
+            newsTitleContainer.classList.add('content__text-title')
+            newsTitleContainer.textContent =  newsTitle 
+    
+            const newsTextContainer = document.createElement('div')
+            newsTextContainer.classList.add('content__text')
+            newsTextContainer.appendChild(newsDateContainer)
+            newsTextContainer.appendChild(newsTitleContainer)
+    
+            const newsImageSrc = document.createElement('img')
+            newsImageSrc.classList.add('content__image')
+            newsImageSrc.src = `${newsImage}`
+
+            const newsLinkContainer = document.createElement('a')
+            newsLinkContainer.classList.add('content__link')
+            newsLinkContainer.href = newsLink
+            newsLinkContainer.setAttribute('target', '_blank')
+            newsLinkContainer.appendChild(newsImageSrc)
+    
+            const newsArticle = document.createElement('article')
+            newsArticle.classList.add('content__news')
+            
+            newsArticle.appendChild(newsLinkContainer)
+            newsArticle.appendChild(newsTextContainer)
+           
+            mainNews.appendChild(newsArticle)
+            console.log(tecNews)
+        }     
+    }
+    
+    catch(error) {
+        console.log(error)
+    }
+}
+
+setNews()*/
